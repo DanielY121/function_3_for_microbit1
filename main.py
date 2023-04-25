88b9cfbe5d6b2b649b858aa3742ccd1cb3646230
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-04-25 21:32:15 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def on_button_pressed_a():
     click += -1
     if click == 1:
         basic.show_string("a")
-        basic.pause(1000)
+        basic.pause(2000)
         basic.clear_screen()
     else:
         pass
@@ -11,8 +11,14 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
     global a
-    if click == 0:
+    if click == 1:
         a += 1
+    elif click == 2:
+        pass
+    elif click == 3:
+        pass
+    elif click == 4:
+        pass
     else:
         pass
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
@@ -30,6 +36,31 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 
 a = 0
 click = 0
+n = 0
+m = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
+i = 0
+j = 0
+k = 0
+l = 0
+o = 0
+p = 0
+q = 0
+r = 0
+s = 0
+t = 0
+u = 0
+v = 0
+w = 0
+x = 0
+y = 0
+z = 0
 click = 0
 
 def on_forever():
@@ -39,6 +70,48 @@ def on_forever():
         basic.pause(1000)
         basic.clear_screen()
         a += -1
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
+    elif False:
+        pass
     else:
         pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     click += -1
     if (click == 1) {
         basic.showString("a")
-        basic.pause(1000)
+        basic.pause(2000)
         basic.clearScreen()
     } else {
     	
@@ -11,6 +11,12 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (click == 1) {
         a += 1
+    } else if (click == 2) {
+    	
+    } else if (click == 3) {
+    	
+    } else if (click == 4) {
+    	
     } else {
     	
     }
@@ -25,15 +31,82 @@ input.onButtonPressed(Button.B, function () {
     	
     }
 })
-let click = 0
-click = 0
 let a = 0
+let click = 0
+let z = 0
+let y = 0
+let x = 0
+let w = 0
+let v = 0
+let u = 0
+let t = 0
+let s = 0
+let r = 0
+let q = 0
+let p = 0
+let o = 0
+let l = 0
+let k = 0
+let j = 0
+let i = 0
+let h = 0
+let g = 0
+let f = 0
+let e = 0
+let d = 0
+let c = 0
+let b = 0
+let m = 0
+let n = 0
+click = 0
 basic.forever(function () {
     if (a == 1) {
         basic.showString("Apple")
         basic.pause(1000)
         basic.clearScreen()
         a += -1
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
     } else {
     	
     }
