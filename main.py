e7c1b3ff89fd76996811a057261a89946ae4c109
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2023-04-25 22:37:27 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -14,17 +14,59 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global a
+    global a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
     if click == 1:
         a += 1
     elif click == 2:
-        pass
+        b += 1
     elif click == 3:
-        pass
+        c += 1
     elif click == 4:
-        pass
-    else:
-        pass
+        d += 1
+    elif click == 5:
+        e += 1
+    elif click == 6:
+        f += 1
+    elif click == 7:
+        g += 1
+    elif click == 8:
+        h += 1
+    elif click == 9:
+        i += 1
+    elif click == 10:
+        j += 1
+    elif click == 11:
+        k += 1
+    elif click == 12:
+        l += 1
+    elif click == 13:
+        m += 1
+    elif click == 14:
+        n += 1
+    elif click == 15:
+        o += 1
+    elif click == 16:
+        p += 1
+    elif click == 17:
+        q += 1
+    elif click == 18:
+        r += 1
+    elif click == 19:
+        s += 1
+    elif click == 20:
+        t += 1
+    elif click == 21:
+        u += 1
+    elif click == 22:
+        v += 1
+    elif click == 23:
+        w += 1
+    elif click == 24:
+        x += 1
+    elif click == 25:
+        y += 1
+    elif click == 26:
+        z += 1
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
@@ -38,8 +80,6 @@ def on_button_pressed_b():
         pass
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-a = 0
-click = 0
 z = 0
 y = 0
 x = 0
@@ -52,6 +92,8 @@ r = 0
 q = 0
 p = 0
 o = 0
+n = 0
+m = 0
 l = 0
 k = 0
 j = 0
@@ -63,59 +105,115 @@ e = 0
 d = 0
 c = 0
 b = 0
-m = 0
-n = 0
+a = 0
+click = 0
 click = 0
 
 def on_forever():
-    global a
+    global a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z
     if a == 1:
         basic.show_string("Apple")
         basic.pause(1000)
         basic.clear_screen()
         a += -1
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    elif False:
-        pass
-    else:
-        pass
+    elif b == 1:
+        basic.pause(1000)
+        basic.clear_screen()
+        b += -1
+    elif c == 1:
+        basic.pause(1000)
+        basic.clear_screen()
+        c += -1
+    elif d == 1:
+        basic.pause(1000)
+        basic.clear_screen()
+        d += -1
+    elif e == 1:
+        basic.pause(1000)
+        basic.clear_screen()
+        e += -1
+    elif f == 6:
+        basic.pause(1000)
+        basic.clear_screen()
+        f += -1
+    elif g == 7:
+        basic.pause(1000)
+        basic.clear_screen()
+        g += -1
+    elif h == 8:
+        basic.pause(1000)
+        basic.clear_screen()
+        h += -1
+    elif i == 9:
+        basic.pause(1000)
+        basic.clear_screen()
+        i += -1
+    elif j == 10:
+        basic.pause(1000)
+        basic.clear_screen()
+        j += -1
+    elif k == 11:
+        basic.pause(1000)
+        basic.clear_screen()
+        k += -1
+    elif l == 12:
+        basic.pause(1000)
+        basic.clear_screen()
+        l += -1
+    elif m == 13:
+        basic.pause(1000)
+        basic.clear_screen()
+        m += -1
+    elif n == 14:
+        basic.pause(1000)
+        basic.clear_screen()
+        n += -1
+    elif o == 15:
+        basic.pause(1000)
+        basic.clear_screen()
+        o += -1
+    elif p == 16:
+        basic.pause(1000)
+        basic.clear_screen()
+        p += -1
+    elif q == 17:
+        basic.pause(1000)
+        basic.clear_screen()
+        q += -1
+    elif r == 18:
+        basic.pause(1000)
+        basic.clear_screen()
+        r += -1
+    elif s == 19:
+        basic.pause(1000)
+        basic.clear_screen()
+        s += -1
+    elif t == 20:
+        basic.pause(1000)
+        basic.clear_screen()
+        t += -1
+    elif u == 21:
+        basic.pause(1000)
+        basic.clear_screen()
+        u += -1
+    elif v == 22:
+        basic.pause(1000)
+        basic.clear_screen()
+        v += -1
+    elif w == 23:
+        basic.pause(1000)
+        basic.clear_screen()
+        w += -1
+    elif x == 24:
+        basic.pause(1000)
+        basic.clear_screen()
+        x += -1
+    elif y == 25:
+        basic.pause(1000)
+        basic.clear_screen()
+        y += -1
+    elif z == 26:
+        basic.pause(1000)
+        basic.clear_screen()
+        z += -1
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -16,13 +16,55 @@ input.onButtonPressed(Button.AB, function () {
     if (click == 1) {
         a += 1
     } else if (click == 2) {
-        c = 0
+        b += 1
     } else if (click == 3) {
-    	
+        c += 1
     } else if (click == 4) {
-    	
-    } else {
-    	
+        d += 1
+    } else if (click == 5) {
+        e += 1
+    } else if (click == 6) {
+        f += 1
+    } else if (click == 7) {
+        g += 1
+    } else if (click == 8) {
+        h += 1
+    } else if (click == 9) {
+        i += 1
+    } else if (click == 10) {
+        j += 1
+    } else if (click == 11) {
+        k += 1
+    } else if (click == 12) {
+        l += 1
+    } else if (click == 13) {
+        m += 1
+    } else if (click == 14) {
+        n += 1
+    } else if (click == 15) {
+        o += 1
+    } else if (click == 16) {
+        p += 1
+    } else if (click == 17) {
+        q += 1
+    } else if (click == 18) {
+        r += 1
+    } else if (click == 19) {
+        s += 1
+    } else if (click == 20) {
+        t += 1
+    } else if (click == 21) {
+        u += 1
+    } else if (click == 22) {
+        v += 1
+    } else if (click == 23) {
+        w += 1
+    } else if (click == 24) {
+        x += 1
+    } else if (click == 25) {
+        y += 1
+    } else if (click == 26) {
+        z += 1
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -35,9 +77,6 @@ input.onButtonPressed(Button.B, function () {
     	
     }
 })
-let a = 0
-let click = 0
-let c = 0
 let z = 0
 let y = 0
 let x = 0
@@ -50,6 +89,8 @@ let r = 0
 let q = 0
 let p = 0
 let o = 0
+let n = 0
+let m = 0
 let l = 0
 let k = 0
 let j = 0
@@ -59,10 +100,10 @@ let g = 0
 let f = 0
 let e = 0
 let d = 0
-c = 0
+let c = 0
 let b = 0
-let m = 0
-let n = 0
+let a = 0
+let click = 0
 click = 0
 basic.forever(function () {
     if (a == 1) {
@@ -70,49 +111,111 @@ basic.forever(function () {
         basic.pause(1000)
         basic.clearScreen()
         a += -1
-    } else if (a == 2) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else {
-    	
+    } else if (b == 1) {
+        basic.showString("Butterfly")
+        basic.pause(1000)
+        basic.clearScreen()
+        b += -1
+    } else if (c == 1) {
+        basic.showString("Cat")
+        basic.pause(1000)
+        basic.clearScreen()
+        c += -1
+    } else if (d == 1) {
+        basic.showString("Dog")
+        basic.pause(1000)
+        basic.clearScreen()
+        d += -1
+    } else if (e == 1) {
+        basic.showString("Elephant")
+        basic.pause(1000)
+        basic.clearScreen()
+        e += -1
+    } else if (f == 6) {
+        basic.showString("Friend")
+        basic.pause(1000)
+        basic.clearScreen()
+        f += -1
+    } else if (g == 7) {
+        basic.showString("Friend")
+        basic.pause(1000)
+        basic.clearScreen()
+        g += -1
+    } else if (h == 8) {
+        basic.pause(1000)
+        basic.clearScreen()
+        h += -1
+    } else if (i == 9) {
+        basic.pause(1000)
+        basic.clearScreen()
+        i += -1
+    } else if (j == 10) {
+        basic.pause(1000)
+        basic.clearScreen()
+        j += -1
+    } else if (k == 11) {
+        basic.pause(1000)
+        basic.clearScreen()
+        k += -1
+    } else if (l == 12) {
+        basic.pause(1000)
+        basic.clearScreen()
+        l += -1
+    } else if (m == 13) {
+        basic.pause(1000)
+        basic.clearScreen()
+        m += -1
+    } else if (n == 14) {
+        basic.pause(1000)
+        basic.clearScreen()
+        n += -1
+    } else if (o == 15) {
+        basic.pause(1000)
+        basic.clearScreen()
+        o += -1
+    } else if (p == 16) {
+        basic.pause(1000)
+        basic.clearScreen()
+        p += -1
+    } else if (q == 17) {
+        basic.pause(1000)
+        basic.clearScreen()
+        q += -1
+    } else if (r == 18) {
+        basic.pause(1000)
+        basic.clearScreen()
+        r += -1
+    } else if (s == 19) {
+        basic.pause(1000)
+        basic.clearScreen()
+        s += -1
+    } else if (t == 20) {
+        basic.pause(1000)
+        basic.clearScreen()
+        t += -1
+    } else if (u == 21) {
+        basic.pause(1000)
+        basic.clearScreen()
+        u += -1
+    } else if (v == 22) {
+        basic.pause(1000)
+        basic.clearScreen()
+        v += -1
+    } else if (w == 23) {
+        basic.pause(1000)
+        basic.clearScreen()
+        w += -1
+    } else if (x == 24) {
+        basic.pause(1000)
+        basic.clearScreen()
+        x += -1
+    } else if (y == 25) {
+        basic.pause(1000)
+        basic.clearScreen()
+        y += -1
+    } else if (z == 26) {
+        basic.pause(1000)
+        basic.clearScreen()
+        z += -1
     }
 })
